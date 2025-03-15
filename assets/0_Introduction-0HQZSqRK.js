7ae1037a84fb04da1269956b79b7f6c12ec7283b
sakupi01 <sakupi01@users.noreply.github.com>
2025-03-15 02:54:30 +0000

    deploy: 6f7f9b7b59455640c554d7ca329b856a69a0c48c

---
+++ assets/0_Introduction-0HQZSqRK.js
@@ -0,0 +1,4 @@
+import{ae as o,af as i}from"./index-pEnUwu3Z.js";import{useMDXComponents as e}from"./index-CvHmbAqs.js";import"./iframe-D1wZicjI.js";import"../sb-preview/runtime.js";import"./index-wSCK2UMg.js";import"./index-DTrCitTc.js";import"./index-Dr5aR7dm.js";import"./index-Cu4lwwaE.js";import"./index-Drb8G5KN.js";import"./index-DrFu-skq.js";function r(t){const n={h1:"h1",...e(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Introduction"}),`
+`,o.jsx(n.h1,{id:"sakupi01com--colophon",children:"sakupi01.com | COLOPHON"}),`
+`,`
+`]})}function j(t={}){const{wrapper:n}={...e(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(r,{...t})}):r(t)}export{j as default};
