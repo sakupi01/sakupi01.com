1b405e55e36ad1968f98460289582fc8b9a9164f
sakupi01 <sakupi01@users.noreply.github.com>
2025-04-08 13:22:51 +0000

    deploy: 401895cdaa39c9433f17db2929634693baa1dccb

--- assets/0_Introduction-7uNoCGU4.js
+++
@@ -1,4 +0,0 @@
-import{ae as o,af as i}from"./index-83AuOMRJ.js";import{useMDXComponents as e}from"./index-B2sDn-xi.js";import"./iframe-BO_E8ewV.js";import"../sb-preview/runtime.js";import"./index-CE1A8OJQ.js";import"./index-C8yqGtoN.js";import"./index-D8eWxRfe.js";import"./index-Cu4lwwaE.js";import"./index-Drb8G5KN.js";import"./index-DrFu-skq.js";function r(t){const n={h1:"h1",...e(),...t.components};return o.jsxs(o.Fragment,{children:[o.jsx(i,{title:"Introduction"}),`
-`,o.jsx(n.h1,{id:"sakupi01com--colophon",children:"sakupi01.com | COLOPHON"}),`
-`,`
-`]})}function j(t={}){const{wrapper:n}={...e(),...t.components};return n?o.jsx(n,{...t,children:o.jsx(r,{...t})}):r(t)}export{j as default};
