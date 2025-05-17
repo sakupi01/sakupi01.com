7d2d26429a84892bf58e7472ff8fb1e84493156a
saku-1101 <sakupi1101@outlook.jp>
2025-05-17 10:43:31 +0900

    add: blog data

---
+++ apps/blog.sakupi01.com/src/pages/blog.json.ts
@@ -0,0 +1,16 @@
+import { marked } from "marked";
+import { allHouseBlogPosts } from "../utils/contents";
+
+export async function GET() {
+  const items = await Promise.all(
+    allHouseBlogPosts.map(async (post) => ({
+      title: post.data.title,
+      pubDate: post.data.update,
+      description: post.data.excerpt,
+      link: `https://blog.sakupi01.com/${post.slug}/`,
+      content: await marked.parse(post.body),
+      tags: post.data.tags,
+    })),
+  );
+  return new Response(JSON.stringify(items));
+}
